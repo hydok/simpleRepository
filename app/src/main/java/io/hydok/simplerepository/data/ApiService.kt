8ee1abffd2d4dc66689763db1bb1f98de7357c30
hydok <hydok@naver.com>
2022-09-28 13:25:02 +0900

    api add

--- app/src/main/java/io/hydok/simplerepository/data/ApiService.kt
+++ app/src/main/java/io/hydok/simplerepository/data/ApiService.kt
@@ -1,4 +1,11 @@
 package io.hydok.simplerepository.data
 
+import io.hydok.simplerepository.model.Cat
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET("v1/images/search?limit=10")
+    suspend fun getBasicCats(): List<Cat>
+
 }
